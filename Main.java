e575a820c5c5f6b39b26e8b979fdc81de2e148d6
Robert Rieger <robertrieger@zipcoders-MacBook-Pro-96.local>
2019-05-21 18:25:02 -0400

    Completed Alice and Bob Lab

--- Main.java
+++ Main.java
@@ -1,9 +1,22 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
+        
 
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter your name");
+        String userInput = in.nextLine();
+        
+        
+        if(userInput.equalsIgnoreCase("alice") || userInput.equalsIgnoreCase("bob") ) {
+            System.out.println("Hello " + userInput);
+        } else {
+            System.out.println("Hello random person you're not Bob or Alice");
+
+        }
     }
 }
